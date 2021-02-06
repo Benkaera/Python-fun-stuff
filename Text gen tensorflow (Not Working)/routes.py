fdffdac791d34f6bb4d9ea555d52274455150b44
Benkaera <50444630+Benkaera@users.noreply.github.com>
2021-02-06 10:30:57 +0300

    Add files via upload

---
+++ Text gen tensorflow (Not Working)/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template, request, redirect
+# from .generator import ai
+
+generator = Blueprint('generator', __name__)
+
+@generator.route('/')
+def index():
+    return render_template('index.html')
+
+@generator.route('/analyze', methods=['POST'])
+def analyze():
+    title = request.form['title']
+    text = ai.generate_text(title)
+
+    return render_template('index.html', text=text)
+
+
